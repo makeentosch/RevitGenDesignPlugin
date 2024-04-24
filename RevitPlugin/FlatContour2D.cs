dbf39c2c32595a883bbd56b5f04a49faf7081de6
makeentosch <artem.irk45@gmail.com>
2024-04-24 23:37:33 +0500

    create: folder of generative algorithms create: class of flat contour in 2D view create: generate.cs

---
+++ RevitPlugin/FlatContour2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitPlugin
+{
+    internal class FlatContour2D
+    {
+        public readonly IGeometricShape2D GeometricShape;
+        public readonly Side2D SideWithDoor;
+        public readonly Side2D SideWithWindow;
+        public readonly string Name;
+
+        public FlatContour2D(IGeometricShape2D geometricShape, Side2D sideWithDoor, Side2D sideWithWindow, string name = "")
+        {
+            GeometricShape = geometricShape;
+            SideWithDoor = sideWithDoor;
+            SideWithWindow = sideWithWindow;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+    }
+}
